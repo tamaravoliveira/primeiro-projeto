e94ac6e2207b509134f8cac31ca7d621f433383e
Tamara Oliveira <tamaravaoliveira@gmail.com>
2022-05-14 11:09:24 -0300

    creating modules - aula 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'; //decorator importado
+import { BrowserModule } from '@angular/platform-browser'; //prepara a aplicação para ser executada no browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosModule } from './cursos/cursos.module';
 
-@NgModule({
-  declarations: [
+@NgModule({ //decorar a classe, que representa um módulo
+  declarations: [ //lista de todos os componentes, diretivas e pipes que utilizaremos nesse módulo
     AppComponent,
     MeuPrimeiroComponent,
     MeuPrimeiro2Component
   ],
-  imports: [
+  imports: [ //lista outros módulos que vamos utilizar ou que está presente dentro de algum componente
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule
   ],
-  providers: [],
+  providers: [], //onde fica os serviços disponíveis para os componentes declarados nesse módulo
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { } //declaração da classe
